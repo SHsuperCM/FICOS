973fd9468f4b7aa08f70141d6f6dfaf2de937bc9
SHsuperCM <shsupercm@gmail.com>
2025-08-11 18:23:25 +0300

    OS file base

---
+++ ficos/os.lua
@@ -0,0 +1,21 @@
+---
+--- Created by SHsuperCM.
+--- DateTime: 11/08/2025 14:53
+---
+
+os = {
+    version = {
+        major = 1,
+        minor = 0,
+        patch = 0
+    }
+}
+
+function os:startup()
+    print("~ Fic OS v" .. self.version.major .. "." .. self.version.minor .. "." .. self.version.patch .. " by SHsuperCM")
+    while true do
+        sleep(1)
+    end
+end
+
+return os
