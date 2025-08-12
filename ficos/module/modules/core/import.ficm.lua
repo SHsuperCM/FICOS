844ad7532e4cc673fef6d571c6aa506658b85f91
SHsuperCM <shsupercm@gmail.com>
2025-08-12 09:41:53 +0300

    Test stub for import module

---
+++ ficos/module/modules/core/import.ficm.lua
@@ -0,0 +1,13 @@
+---
+--- Created by SHsuperCM.
+--- DateTime: 12/08/2025 8:28
+---
+
+---@type FICModule
+local module = FICModule.new("core.import", 1, 0, 0)
+
+
+print("loaded import!")
+
+
+return module
