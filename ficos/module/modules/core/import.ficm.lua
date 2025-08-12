b534c87e2a4affaf27d12683e42ef42871823e7d
SHsuperCM <shsupercm@gmail.com>
2025-08-12 12:53:58 +0300

    Prevent import loop

--- ficos/module/modules/core/import.ficm.lua
+++ ficos/module/modules/core/import.ficm.lua
@@ -6,9 +6,20 @@
 ---@type FICModule
 local module = FICModule.new("core.import", 1, 0, 0)
 
+module.importing = {}
+
 ---@param id string id of the requested module
 ---@return FICModule
 function import(id)
+    id = id:lower()
+
+    if module.importing[id] ~= nil then
+        print("Import loop detected through module " .. id)
+        error("Import loop detected through module " .. id)
+    end
+
+    module.importing[id] = true
+
     local loadedModule
     local loaded, err = pcall(function()
         loadedModule = os.moduleManager:load(id)
@@ -18,6 +29,8 @@ function import(id)
         print("Could not import module " .. id)
         error(err)
     end
+
+    module.importing[id] = nil
 
     return loadedModule
 end
