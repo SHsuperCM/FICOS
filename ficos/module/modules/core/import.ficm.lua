ad7527c6e69e34f8d87afaa37c2540d85cb0d4ed
SHsuperCM <shsupercm@gmail.com>
2025-08-12 10:46:17 +0300

    Import implementation

--- ficos/module/modules/core/import.ficm.lua
+++ ficos/module/modules/core/import.ficm.lua
@@ -6,8 +6,20 @@
 ---@type FICModule
 local module = FICModule.new("core.import", 1, 0, 0)
 
+---@param id string id of the requested module
+---@return FICModule
+function import(id)
+    local loadedModule
+    local loaded, err = pcall(function()
+        loadedModule = os.moduleManager:load()
+    end)
 
-print("loaded import!")
+    if not loaded then
+        print("Could not import module " .. id)
+        error(err)
+    end
 
+    return loadedModule
+end
 
 return module
