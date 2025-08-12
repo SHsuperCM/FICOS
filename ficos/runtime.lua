2b2276bd778101aedab6355cdcb085ad1a4f6781
SHsuperCM <shsupercm@gmail.com>
2025-08-12 14:03:29 +0300

    Refactored runtime to manage futures by module

--- ficos/runtime.lua
+++ ficos/runtime.lua
@@ -4,12 +4,58 @@
 ---
 
 local runtime = {
-    shouldRun = true
+    shouldRun = true,
+    futures = { }
 }
+
+---@param module string|FICModule
+---@param future Future
+function runtime:add(module, future)
+    if type(module) ~= "string" then
+        module = module.id
+    end
+
+    self.futures[future] = module
+end
+
+---@param future Future
+function runtime:remove(future)
+    self.futures[future] = nil
+end
+
+---@param removalModule string|FICModule
+function runtime:removeByModule(removalModule)
+    if type(removalModule) ~= "string" then
+        removalModule = removalModule.id
+    end
+
+    local removal = { }
+
+    for future, module in pairs(self.futures) do
+        if module == removalModule then
+            removal[#removal + 1] = future
+        end
+    end
+
+    for _, future in pairs(removal) do
+        self:remove(future)
+    end
+end
 
 function runtime:run()
     while self.shouldRun do
-        future.run()
+        local finished = { }
+
+        for future, module in pairs(self.futures) do
+            if future:poll() then
+                finished[#finished + 1] = future
+            end
+        end
+
+        for _, future in pairs(finished) do
+            self:remove(future)
+        end
+
         coroutine.yield()
     end
 end
